b22bd5ac68e89a67dfd30b41b1490d4e6b32c422
Jefferson Sousa <jejefferson57@gmail.com>
2026-03-02 16:30:24 -0300

    feat: create initial entities - income and expense

---
+++ src/main/java/com/jeffssousa/fluxo/entities/Expense.java
@@ -0,0 +1,30 @@
+package com.jeffssousa.fluxo.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_expenses")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID expenseId;
+
+    private String description;
+    private BigDecimal amount;
+    private LocalDateTime transactionDate;
+    private boolean status; // paid/ not paid
+
+}
---
+++ src/main/java/com/jeffssousa/fluxo/entities/Income.java
@@ -0,0 +1,30 @@
+package com.jeffssousa.fluxo.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_incomes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Income {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID incomeId;
+
+    private String description;
+    private BigDecimal amount;
+    private LocalDateTime transactionDate;
+    private boolean status; // received/ not received
+
+}
